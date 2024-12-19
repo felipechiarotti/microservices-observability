80f8ca382a77d11cbde886cdf42534e87c0a0f55
Felipe Chiarotti <felipechi97@gmail.com>
2024-12-19 12:44:29 -0300

    Logging with elasticksearch

---
+++ BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
+
+namespace Common.Logging
+{
+    public static class SeriLogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+            (context, configuration) =>
+            {
+                configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Console()
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .ReadFrom.Configuration(context.Configuration);
+            };
+
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureElasticSearch =>
+            (context, configuration) =>
+            {
+                Configure(context, configuration);
+                configuration.WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticSearchConfiguration:Uri"]))
+                    {
+                        IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",
+                        AutoRegisterTemplate = true,
+                        NumberOfShards = 2,
+                        NumberOfReplicas = 1
+                    }
+                );
+            };
+    }
+}
